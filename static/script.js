d31458cd0b56e5cd9d7ab8c848ef02da11c33720
hilalabror32-jpg <bucuanyeb@gmail.com>
2026-05-05 12:58:35 +0700

    first file

--- static/script.js
+++ static/script.js
@@ -6,11 +6,13 @@ function go(n) {
   document.getElementById("s" + n).classList.remove("hide");
 }
 
-// 🔥 tombol NO kabur
+// tombol NO kabur 😈
 function noKabur() {
   let btn = document.getElementById("noBtn");
 
   btn.style.position = "absolute";
-  btn.style.top = Math.random() * 300 + "px";
-  btn.style.left = Math.random() * 300 + "px";
+  btn.style.top = Math.random() * window.innerHeight + "px";
+  btn.style.left = Math.random() * window.innerWidth + "px";
+
+  btn.innerText = "YAKIN? 😈";
 }
